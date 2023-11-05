1bad0306eb0a64cde742f951c36a75291d010b80
Vlad <vladsfa.personal@gmail.com>
2023-11-05 11:34:21 +0200

    addMap

--- src/app/(tabs)/map.tsx
+++ src/app/(tabs)/map.tsx
@@ -1,9 +1,16 @@
 import {Text, View} from "react-native";
+import MapView from "react-native-maps";
+import {Tabs} from "expo-router";
+import {COLORS} from "../../constants/theme";
 
 export default function Map(){
     return (
         <View>
-            <Text>Map</Text>
+            <Tabs.Screen options={{
+                headerTransparent: true,
+                headerTintColor: COLORS.white
+            }}/>
+            <MapView style={{width: '100%', height: '100%'}}/>
         </View>
     )
 }
