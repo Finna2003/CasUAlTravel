63bd8e2c492fd9f128efe4d11f83532470aad576
Inna Fisyuk <inna.fisyuk.0203@gmail.com>
2023-11-06 00:00:02 +0200

    edit login register pages

--- src/app/(auth)/register.tsx
+++ src/app/(auth)/register.tsx
@@ -1,13 +1,56 @@
-import {View} from "react-native";
-import {Link} from "expo-router";
+import {Pressable, Text, TextInput, View} from "react-native";
+import {Link, Stack} from "expo-router";
 import {useSession} from "../../auth/sessionProvider";
+import {STYLE} from "../../constants/theme";
+import React from "react";
 
 export default function Register(){
     const {signIn} = useSession();
 
     return (
         <View>
-            <Link href={'/'} onPress={() => signIn()}>Register</Link>
+            <Text style={{...STYLE.defaultBigText,         textAlign: 'center'}}>Заповніть основну</Text>
+            <Text style={{...STYLE.defaultBigText,         textAlign: 'center'}}>інформацію</Text>
+            <TextInput
+                style={{
+                    ...STYLE.defaultPlaceForText,
+                    position: 'absolute',
+                    top: 100,
+                    alignSelf: 'center',
+                }}
+                placeholder="Ім'я"
+            />
+            <TextInput
+                style={{
+                    ...STYLE.defaultPlaceForText,
+                    position: 'absolute',
+                    top: 170,
+                    alignSelf: 'center',
+                }}
+                placeholder="Електронна пошта"
+            />
+            <TextInput
+                style={{
+                    ...STYLE.defaultPlaceForText,
+                    position: 'absolute',
+                    top: 250,
+                    alignSelf: 'center',
+                }}
+                placeholder="Пароль"
+            />
+            <Link href='/' asChild>
+                <Pressable onPress={() => signIn()}
+                           style={{
+                               ...STYLE.defaultButton,
+                               position: 'absolute',
+                               top: 330,
+                               alignSelf: 'center',
+                           }}
+                >
+                    <Text style={{...STYLE.defaultButtonText,         textAlign: 'center'}}>Зареєструватися</Text>
+                </Pressable>
+            </Link>
+            <Link href={'/login'}  style={{...STYLE.defaultLinkText, position: 'absolute', top: 390, textDecorationLine: 'underline', alignSelf: 'center',}}>Вже маєте обліковий запис?</Link>
         </View>
     )
 }
