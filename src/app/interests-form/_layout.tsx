f69c2521bca366a3088bea86dbc5ad6950fe0b6b
Inna Fisyuk <inna.fisyuk.0203@gmail.com>
2023-11-26 22:02:52 +0200

    create question

--- src/app/interests-form/_layout.tsx
+++ src/app/interests-form/_layout.tsx
@@ -1,9 +1,35 @@
 import {Stack} from "expo-router";
+import {COLORS} from "../../constants/theme";
 
 export default function InterestsFormLayout(){
     return (
-        <Stack>
-
+        <Stack
+            screenOptions={{
+                // Ваші налаштування заголовка тут
+                headerBackTitleVisible: false,
+                headerTransparent: true,
+                headerStyle: {
+                },
+                headerShadowVisible: false,
+                headerTintColor: COLORS.black,
+                contentStyle: {
+                    backgroundColor: COLORS.white
+                },
+                fullScreenGestureEnabled: true
+            }}
+        >
+            <Stack.Screen
+                name="welcome"
+                options={{
+                    headerTitle: '' // Порожній заголовок для сторінки "Login"
+                }}
+            />
+            <Stack.Screen
+                name="door1"
+                options={{
+                    headerTitle: '' // Порожній заголовок для сторінки "Login"
+                }}
+            />
         </Stack>
     )
 }
--- src/app/interests-form/welcome.tsx
+++ src/app/interests-form/welcome.tsx
@@ -1,17 +1,46 @@
-import {Text, View} from "react-native";
+import {Image, StyleSheet, Text, View} from "react-native";
 import ProjectButtonDark from "../../components/common/ProjectButtonDark";
 import {router} from "expo-router";
+import {COLORS} from "../../constants/theme";
+import React from "react";
 
 export default function Welcome(){
     return (
-        <View>
-            <Text>
-                Welcome
+        <View style={styles.contentContainer}>
+            <View>
+                <Image
+                    source={require("../../assets/images/photo2.jpg")}
+                    style={styles.img}
+                    resizeMode={"cover"}
+                />
+            </View>
+            <Text style={styles.title}>
+                Пройдіть інтерактивне опитування, {'\n'}
+                щоб створити персоналізований {'\n'}
+                маршрут
             </Text>
-            <ProjectButtonDark text={"Перейти до Door1"} onPress={() => {
-
+            <ProjectButtonDark text={"Розпочати"} onPress={() => {
                 router.push("/interests-form/door1");
             }}/>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    contentContainer: {
+        flex: 0.75,
+        marginBottom: 40,
+        backgroundColor: COLORS.white,
+        justifyContent: "space-between"
+    },
+    img: {
+        marginTop: 120,
+        width: 333,
+        alignSelf: "center",
+        height: 234
+    },
+    title: {
+        fontSize: 20,
+        alignSelf: "center",
+        textAlign: 'center'
+    }
+})
